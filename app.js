2ebc9bfb99b9eccfd9fc3f0cac009a86c7cf7376
Phanu <admin@phanuphanu.com>
2022-07-08 13:17:11 -0700

    Object Methods ES2015

--- app.js
+++ app.js
@@ -7,22 +7,18 @@ function createInstructor(firstName, lastName){
 }
 
 // Computed Property Names ES2015
-let favoriteNumber = 42
+// let favoriteNumber = 42
 
+// const instructor = {
+//   firstName: "Colt",
+//   [favoriteNumber]: "That is my favorite!"
+// }
+
+
+
+// Object Methods ES2015
 const instructor = {
-  firstName: "Colt",
-  [favoriteNumber]: "That is my favorite!"
-}
-
-
-
-// Object Methods
-// var instructor = {
-//     firstName: "Colt",
-//     sayHi: function(){
-//       return "Hi!";
-//     },
-//     sayBye: function(){
-//       return this.firstName + " says bye!";
-//     }
-//   }
+    firstName: "Colt",
+    sayHi(){return "Hi!";},
+    sayBye(){return this.firstName + " says bye!";}
+  }
